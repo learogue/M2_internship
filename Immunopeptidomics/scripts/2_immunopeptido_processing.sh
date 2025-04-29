e50b54d9546de39b1e862924137359edb65785cf
Ubuntu <ubuntu@machine86a19abf-c828-42c9-a61d-bcf14f1ae12c.oscarlocal>
2025-04-29 11:40:32 +0000

    updated

--- Immunopeptidomics/scripts/2_immunopeptido_processing.sh
+++ Immunopeptidomics/scripts/2_immunopeptido_processing.sh
@@ -18,10 +18,10 @@ awk -F'\t' 'NR > 1 {print ">" $1 "\n" $6}' results/table_peptides_cancer_human.t
 makeblastdb -in data/proteine_seq_targeted_cta.fasta -dbtype prot -out data/db/targeted_cta_db
 
 # Align petides with sequences of selected genes
-blastp -query results/seq_pep.fasta -db data/db/targeted_cta_db -outfmt '6 qseqid qlen qstart qend sseqid slen sstart send length bitscore evalue pident mismatch' -evalue 0.01 -num_threads 6 -out results/results_blastp_mismatch_eval_0_01.tsv
+blastp -query results/seq_pep.fasta -db data/db/targeted_cta_db -outfmt '6 qseqid qlen qstart qend sseqid slen sstart send length bitscore evalue pident mismatch' -num_threads 6 -out results/results_blastp.tsv
 
 # Select hits with 0 or 1 mismatchs
-awk -F'\t' '($13 == 0 || $13 == 1)' results/results_blastp_mismatch_eval_0_01.tsv > results/selected_results_blastp_mismatch_eval_0_01.tsv
+awk -F'\t' '($13 == 0 || $13 == 1)' results/results_blastp.tsv > results/selected_results_blastp_mismatch.tsv
 
 # Take gene names and sseid
 grep '^>' proteine_seq_targeted_cta.fasta | tr -d '>' | awk '{
@@ -32,3 +32,9 @@ grep '^>' proteine_seq_targeted_cta.fasta | tr -d '>' | awk '{
         }
     }
 }' > entry_name_gene_name.tsv 
+
+# Create fasta with peptide sequences from blastp results
+awk 'NR==FNR {info[$1]=$5; next} /^>/ {id=substr($0,2); 
+    show=(id in info)} show {print (substr($0,2) in info ? ">" id " " info[id] : $0); getline; print}' 
+    selected_results_blastp_mismatch.tsv seq_pep.fasta > seq_pep_selected_blastp.fasta
+
